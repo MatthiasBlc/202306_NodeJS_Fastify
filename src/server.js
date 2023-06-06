c81fce4b306963de196a5afb298106e20b743079
MatthiasBlc <mat.bouloc@gmail.com>
2023-06-06 15:17:19 +0200

    adding a template engine -ejs

--- src/server.js
+++ src/server.js
@@ -1,1 +1,38 @@
-console.log("new");
+import { fastify } from "fastify";
+import { fastifyView } from "@fastify/view";
+import ejs from "ejs";
+
+const app = fastify();
+
+app.register(fastifyView, {
+  engine: {
+    ejs,
+  },
+});
+
+app.get("/", (req, res) => {
+  const posts = [
+    {
+      title: "new title",
+      content: "new content",
+    },
+    {
+      title: "new title 2",
+      content: "new content 2",
+    },
+  ];
+
+  res.view("templates/index.ejs", {
+    posts,
+  });
+});
+
+const start = async () => {
+  try {
+    await app.listen({ port: 3000 });
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+start();
