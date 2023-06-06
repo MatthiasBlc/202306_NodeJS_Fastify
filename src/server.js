6f1c8eb54a9232d7f553dcb4999a9dc4d341f1a9
MatthiasBlc <mat.bouloc@gmail.com>
2023-06-06 16:10:25 +0200

    adding publics files system

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import { fastify } from "fastify";
 import { fastifyView } from "@fastify/view";
 import ejs from "ejs";
+import { fastifyStatic } from "@fastify/static";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
 
 const app = fastify();
+const rootDir = dirname(dirname(fileURLToPath(import.meta.url)));
 
 app.register(fastifyView, {
   engine: {
     ejs,
   },
+});
+
+app.register(fastifyStatic, {
+  root: join(rootDir, "public"),
 });
 
 app.get("/", (req, res) => {
